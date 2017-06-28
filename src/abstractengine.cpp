f7c082816eaa91352a20be085788af4d486923c2
Pablo Rodenas <pablo.rodenas@bsc.es>
2017-06-28 17:51:03 +0200

    PBS changes in site but not fully tested. All files with license GPLv3 Minor bug fixes

--- src/abstractengine.cpp
+++ src/abstractengine.cpp
@@ -1,3 +1,21 @@
+/* 
+ * This file is part of GREASY software package
+ * Copyright (C) by the BSC-Support Team, see www.bsc.es
+ * 
+ * GREASY is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * GREASY is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GREASY. If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
 
 #ifdef MPI_ENGINE
     #include "mpiengine.h"
@@ -58,7 +76,10 @@ AbstractEngine::AbstractEngine ( string filename ) {
   
   taskFile = filename;
   string jobid="";
-  jobid=getenv(JOBID);
+  char const * tmp = getenv(JOBID);
+  
+  if ( tmp == NULL ) jobid="undefined";
+  else jobid=string(tmp);
 
   if(!jobid.empty()) jobid="-" + jobid;
 
